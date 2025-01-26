b380a23f471baa1b314e04c6f19c52b51dac295c
Zachary Anderson <zca721@uw.edu>
2025-01-25 18:00:36 -0800

    Added Background

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./Sprite_Sheet_Rain.png");
+// ASSET_MANAGER.queueDownload("./Sprite_Sheet_Rain.png");
 ASSET_MANAGER.queueDownload("./Rain_Horizontal_Walk.png");
+ASSET_MANAGER.queueDownload("./Gas_Station_1.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
--- rain.js
+++ rain.js
@@ -1,7 +1,8 @@
 class Rain {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprite_Sheet_Rain.png"), 0, 0, 64, 64, 32, 0.15);
+        // this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprite_Sheet_Rain.png"), 0, 0, 64, 64, 32, 0.15);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./Gas_Station_1.png"), 0, 0, 320, 180, 1, 0.15);
         this.horizontal = new Animator(ASSET_MANAGER.getAsset("./Rain_Horizontal_Walk.png"), 0, 0, 64, 64, 4, 0.15);
         this.x = 0;
         this.y = 366;
@@ -16,8 +17,8 @@ class Rain {
     };
 
     draw(ctx) {
-        // this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        this.animator.drawFrame(this.game.clockTick, ctx, 100, 150);
+        // ctx.drawImage(ASSET_MANAGER.getAsset("./Gas_Station_1.png"),0 ,0);
         this.horizontal.drawFrame(this.game.clockTick, ctx, this.x, this.y)
-        // ctx.drawImage(ASSET_MANAGER.getAsset("./Main_Character_Sprite_Sheet.png"),0 ,0);
     };
 }
